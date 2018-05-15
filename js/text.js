a280417cab7c5a6eb02ae564de0268e8d6f39ba0
yoneyama <yoneyama@wize-net.co.jp>
2018-05-15 14:21:20 +0900

    q48終了

--- js/text.js
+++ js/text.js
@@ -1,3 +1,12 @@
 (function(){
+  function funcColorName(colorName){
+    $("#q48").css('color', colorName);
+  }
 
+  $("button").click(function(){
+    funcColorName('red');
+  });
 })();
+// 色名（例：red）を代入した変数名「colorName」を引数に入れたら
+// id名「q48」の文字色をcolorName変数の色に変えるユーザー定義関数名「funcColorName」を作成
+// class名「button」をクリックしたら「funcColorName」関数を発動せよ
