bf2b8e22dd965f98699dd10312976035e054d656
yoneyama <yoneyama@wize-net.co.jp>
2018-05-15 11:10:11 +0900

    q47開始

--- js/text.js
+++ js/text.js
@@ -1,36 +1,6 @@
 (function(){
-  $("#q46_button").click(function(){
-      var val = Number($("#q46_val").val());
-      var subject = Number($("#subject").val());
-      var counting = $("#q46_select option:selected").val();
-
-      switch (counting) {
-        case "addition": funcAddition(val, subject);
-          break;
-        case "subtraction": funcSubtraction(val, subject);
-          break;
-        case "multiplication": funcMultiplication(val, subject);
-          break;
-        case "division": funcDivision(val, subject);
-          break;
-      }
-  });
-      console.log(q46_answer);
-  function funcAddition(val,subject){
-        var answer = (val + subject);
-        $("p#q46_answer").append(answer);
-      }
-  function funcSubtraction(val,subject){
-        var answer = (val - subject);
-        $("p#q46_answer").append(answer);
-        }
-  function funcMultiplication(val,subject){
-        var answer = (val * subject);
-        $("p#q46_answer").append(answer);
-        }
-  function funcDivision(val,subject){
-        var answer = (val / subject);
-        $("p#q46_answer").append(answer);
-    }
 
   })();
+
+//id名「q47」要素にある”ID”のついたdiv要素だけを配列に代入し、
+//そのid名を取得しclass名「q47」のp要素に出力せよ
