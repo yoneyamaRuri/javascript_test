b7cb3d19ad019fc6c381f12b6921cac073ddd284
yoneyama <yoneyama@wize-net.co.jp>
2018-05-15 15:41:22 +0900

    q49をクリアにした。

--- js/text.js
+++ js/text.js
@@ -1,10 +1,5 @@
 (function(){
-  $("#q49 li").eq(2).cluck(function(){
-    setTimeout(function(){
-      $("#q49").eq(2).replaceWith('<li>『リンクは偽物(フェイク)★-_-；』</li>');
-    } );
-    return false;
-  });
-
   
+
+
 })();
