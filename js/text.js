a9f9c024357b5d7d64b1fd8cc2ee121d07b8d551
yoneyama <yoneyama@wize-net.co.jp>
2018-05-15 14:31:50 +0900

    q48をクリアにした

--- js/text.js
+++ js/text.js
@@ -1,12 +1,5 @@
 (function(){
-  function funcColorName(colorName){
-    $("#q48").css('color', colorName);
-  }
 
-  $("button").click(function(){
-    funcColorName('red');
-  });
 })();
-// 色名（例：red）を代入した変数名「colorName」を引数に入れたら
-// id名「q48」の文字色をcolorName変数の色に変えるユーザー定義関数名「funcColorName」を作成
-// class名「button」をクリックしたら「funcColorName」関数を発動せよ
+// id名「q49」のul要素のリストの３番目がクリックされた２秒後に
+// 対象リストを『リンクは偽物(フェイク)★-_-；』に置き換えなさい
